d7f1cd7697d6dc65c9d3d0696f38b4ba043a57fe
robinparadise <robinchogiles@gmail.com>
2015-06-10 00:28:34 +0200

    add bugger and projetile resource

--- template/src/resource.js
+++ template/src/resource.js
@@ -2,30 +2,23 @@ var s_HelloWorld = "HelloWorld.jpg";
 var s_CloseNormal = "CloseNormal.png";
 var s_CloseSelected = "CloseSelected.png";
 var s_mainPlayer = "turtle.png";
+var s_bugger = "bugger.png";
+var s_projectile = "projectile.png";
 
 var res = {
   helloWorld: s_HelloWorld,
   closeNormal: s_CloseNormal,
   s_closeSelected: s_CloseSelected,
   mainPlayer: "turtle.png",
-  bugger: "bugger.png"
+  bugger: "bugger.png",
+  projectile: "projectile.png"
 }
 
 var g_resources = [
-    //image
     s_HelloWorld,
     s_CloseNormal,
     s_CloseSelected,
     s_mainPlayer,
-    res.bugger
-
-    //plist
-
-    //fnt
-
-    //tmx
-
-    //bgm
-
-    //effect
+    s_bugger,
+    s_projectile
 ];
