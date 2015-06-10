c36d547b5005bddaa350ab7959c8f7ae57c20870
robinparadise <robinchogiles@gmail.com>
2015-06-10 23:58:01 +0200

    refactor main; set _size variable

--- template/main.js
+++ template/main.js
@@ -1,23 +1,24 @@
 cc.game.onStart = function(){
-    if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
-        document.body.removeChild(document.getElementById("cocosLoading"));
+  if (!cc.sys.isNative && document.getElementById("cocosLoading")) {
+    document.body.removeChild(document.getElementById("cocosLoading"));
+  }
 
-    var designSize = cc.size(800, 480);//cc.size(480, 800);
-    var screenSize = cc.view.getFrameSize();
-    
-    cc.SPRITE_DEBUG_DRAW =  1;
+  _designSize = cc.size(800, 480);//cc.size(480, 800);
+  var screenSize = cc.view.getFrameSize();
+  
+  cc.SPRITE_DEBUG_DRAW =  0;
 
-    if(!cc.sys.isNative && screenSize.height < 800){
-        designSize = cc.size(800, 480); //cc.size(320, 480);
-        cc.loader.resPath = "res/Normal";
-    }else{
-        cc.loader.resPath = "res/HD";
-    }
-    cc.view.setDesignResolutionSize(designSize.width, designSize.height, cc.ResolutionPolicy.SHOW_ALL);
+  if (!cc.sys.isNative && screenSize.height < 800){
+    _designSize = cc.size(800, 480); //cc.size(320, 480);
+    cc.loader.resPath = "res/Normal";
+  } else {
+    cc.loader.resPath = "res/HD";
+  }
+  cc.view.setDesignResolutionSize(_designSize.width, _designSize.height, cc.ResolutionPolicy.SHOW_ALL);
 
-    //load resources
-    cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new MyScene());
-    }, this);
+  //load resources
+  cc.LoaderScene.preload(g_resources, function () {
+    cc.director.runScene(new MyScene());
+  }, this);
 };
 cc.game.run();
